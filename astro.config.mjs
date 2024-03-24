0a7c133aa98eae233cb522daf7b0abaf24d39a59
ricard-gi <153858144+ricard-gi@users.noreply.github.com>
2024-03-24 22:20:26 +0100

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  adapter: netlify()
+  adapter: vercel()
 });
