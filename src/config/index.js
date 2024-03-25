c34686e749eec3aa5eaa5c365a6db98eea3ebe71
ricard-gi <153858144+ricard-gi@users.noreply.github.com>
2024-03-25 10:22:43 +0100

    https

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const IP = "159.223.31.62";
 export default {
     //API_URL: `http://${IP}:3000/api`,
     //IMG_URL: `http://${IP}:3000/img`,
-    API_URL: `http://ricardhernandez.com/api`,
-    IMG_URL: `http://ricardhernandez.com/img`,
+    API_URL: `https://ricardhernandez.com/api`,
+    IMG_URL: `https://ricardhernandez.com/img`,
 
 }
